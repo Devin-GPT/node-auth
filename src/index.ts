f4f0f61a0cd33220dc03b3e54c5d27afc9effbb0
devinGPT <devinGPT@proton.me>
2024-01-07 15:38:59 -0600

    remove unused line in .gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-// import http = require('http');
 import * as http from 'http';
 
 const PORT = process.env.PORT || 3330;
@@ -6,7 +5,7 @@ const PORT = process.env.PORT || 3330;
 http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.write('Hello World!!!!!');
+    res.write('Hello World!!!');
     res.end();
 }).listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
