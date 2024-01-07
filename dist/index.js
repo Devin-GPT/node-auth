3cd0e665508fcf56ab3d09aa5d6ed8d13025a993
BestRingEureka <BestRing-Expert-Support@eurekasoft.com>
2024-01-07 13:57:46 -0600

    various updates to development workflow and container setup

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,13 @@
-const http = require('http');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// import http = require('http');
+const http = require("http");
 const PORT = process.env.PORT || 3330;
-
-
-
 http.createServer((req, res) => {
-
-    if (req.method === 'GET' && req.url === '/') {
-        // Set the status code and content type for JSON response
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-
-        // Send a JSON response
-        res.end(JSON.stringify({ message: 'Hello World' }));
-    } 
-    else {
-        // Handle any other URLs or methods
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Not Found');
-    }
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('Hello World!');
+    res.end();
 }).listen(PORT, () => {
-    console.log(`Serverr running at http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
